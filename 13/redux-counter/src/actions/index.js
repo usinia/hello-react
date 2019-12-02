43a24273262267ff5feb5cde928b7877e10d3f56
usinia <usinia0103@gmail.com>
2019-12-02 23:37:07 +0900

    13장 리덕스로 리액트 애플리케이션 상태 관리 멀티카운터

--- 13/redux-counter/src/actions/index.js
+++ 13/redux-counter/src/actions/index.js
@@ -3,16 +3,30 @@
 */
 import * as types from "./ActionTypes";
 
-export const increment = () => ({
-  type: types.INCREMENT
+// 카운터를 새로 만들 때 기본 색상을 받을 수 있도록 정의
+export const create = color => ({
+  type: types.CREATE,
+  color
 });
 
-export const decrement = () => ({
-  type: types.DECREMENT
+// 맨 마지막 카운터를 삭제하므로 index는 생략
+export const remove = () => ({
+  type: types.REMOVE
+});
+
+export const increment = index => ({
+  type: types.INCREMENT,
+  index
+});
+
+export const decrement = index => ({
+  type: types.DECREMENT,
+  index
 });
 
 // 다른 액션 생성자들과 달리 파라미터를 갖고 있습니다.
-export const setColor = color => ({
+export const setColor = ({ index, color }) => ({
   type: types.SET_COLOR,
+  index,
   color
 });
--- 13/redux-counter/src/index.js
+++ 13/redux-counter/src/index.js
@@ -5,11 +5,19 @@ import "./index.css";
 
 // 리덕스 관련 불러오기
 import { createStore } from "redux";
-import reducers from "./reducers";
+// import reducers from "./reducers";
+import reducers from "./reducers_new";
 import { Provider } from "react-redux";
 
 // 스토어 생성
-const store = createStore(reducers);
+// const store = createStore(reducers);
+
+// 스토어 생성 + Redux DevTools
+const store = createStore(
+  reducers,
+  /* window.devToolsExtension && window.devToolsExtension() // 이전버전 */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
