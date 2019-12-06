1a8082164b65fbfb793df8d11aa46ea14c04aab0
usinia <usinia0103@gmail.com>
2019-12-07 01:07:41 +0900

    15장 리덕스 미들웨어와 외부 데이터 연동 - redux-thunk

--- 15/redux-starter-kit/src/modules/counter.js
+++ 15/redux-starter-kit/src/modules/counter.js
@@ -1,12 +1,27 @@
-import { handleActions, createAction } from 'redux-actions';
+import { handleActions, createAction } from "redux-actions";
 
-const INCREMENT = 'INCREMENT';
-const DECREMENT = 'DECREMENT';
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
 
 export const increment = createAction(INCREMENT);
 export const decrement = createAction(DECREMENT);
 
-export default handleActions({
+export const incrementAsync = () => dispatch => {
+  setTimeout(() => {
+    dispatch(increment());
+  }, 1000);
+};
+
+export const decrementAsync = () => dispatch => {
+  setTimeout(() => {
+    dispatch(decrement());
+  }, 1000);
+};
+
+export default handleActions(
+  {
     [INCREMENT]: (state, action) => state + 1,
     [DECREMENT]: (state, action) => state - 1
-}, 0);
+  },
+  0
+);
