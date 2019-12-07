dca3b1d927f04bf7a8bf3e65c27341c28a8a920b
usinia <usinia0103@gmail.com>
2019-12-08 00:32:08 +0900

    15장 리덕스 미들웨어와 외부 데이터 연동 - redux-pender

--- 15/redux-starter-kit/src/App.js
+++ 15/redux-starter-kit/src/App.js
@@ -5,6 +5,16 @@ import * as counterActions from "./modules/counter";
 import * as postActions from "./modules/post";
 
 class App extends Component {
+  cancelRequest = null;
+
+  handleCancel = () => {
+    "handleCancel", this.cancelRequest;
+    if (this.cancelRequest) {
+      this.cancelRequest();
+      this.cancelRequest = null;
+    }
+  };
+
   /* loadData = () => {
     const { PostActions, number } = this.props;
     PostActions.getPost(number)
@@ -18,7 +28,9 @@ class App extends Component {
   loadData = async () => {
     const { PostActions, number } = this.props;
     try {
-      const response = await PostActions.getPost(number);
+      const p = PostActions.getPost(number);
+      this.cancelRequest = p.cancel;
+      const response = await p;
       console.log(response);
     } catch (e) {
       console.log(e);
@@ -28,6 +40,11 @@ class App extends Component {
   componentDidMount() {
     // postActions.getPost
     this.loadData();
+    window.addEventListener("keyup", e => {
+      if (e.key === "Escape") {
+        this.handleCancel();
+      }
+    });
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -68,8 +85,8 @@ export default connect(
   state => ({
     number: state.counter,
     post: state.post.data,
-    loading: state.post.pending,
-    error: state.post.error
+    loading: state.pender.pending["GET_POST"],
+    error: state.pender.failure["GET_POST"]
   }),
   dispatch => ({
     CounterActions: bindActionCreators(counterActions, dispatch),
--- 15/redux-starter-kit/src/modules/post.js
+++ 15/redux-starter-kit/src/modules/post.js
@@ -1,89 +1,88 @@
-import { handleActions, createAction } from "redux-actions";
 import axios from "axios";
+import { createAction, handleActions } from "redux-actions";
+import { pender, applyPenders } from "redux-pender";
 
 function getPostAPI(postId) {
   return axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`);
 }
 
 const GET_POST = "GET_POST";
-const GET_POST_PENDING = "GET_POST_PENDING";
-const GET_POST_SUCCESS = "GET_POST_SUCCESS";
-const GET_POST_FAILURE = "GET_POST_FAILURE";
-
-const getPostPending = createAction(GET_POST_PENDING);
-const getPostSuccess = createAction(GET_POST_SUCCESS);
-const getPostFailure = createAction(GET_POST_FAILURE);
-
-/* export const getPost = postId => dispatch => {
-  // 먼저 요청이 시작했다는 것을 알립니다.
-  dispatch(getPostPending());
-
-  // 요청을 시작합니다. 여기서 만든 promise를 return해야 나중에 컴포넌트에서 호출할 떄
-  // getPost().then().catch() 할 수 있습니다.
-  return getPostAPI(postId)
-    .then(response => {
-      // 요청이 성공했다면 서버 응답 내용을 payload로 설정하여
-      // GET_POST_SUCCESS 액션을 디스패치합니다.
-      dispatch(getPostSuccess(response));
-
-      // 나중에 getPostAPI.then 을 했을 떄 then에 전달하는 함수에서 response에 접근할 수 있게 합니다.
-      return response;
-    })
-    .catch(error => {
-      // 오류가 발생하면 오류 내용을 payload로 설정하여
-      // GET_POST_FAILURE 액션을 디스패치 합니다.
-      dispatch(getPostFailure);
-
-      // error를 throw하여 이 함수를 실행한 후
-      // 다시 한 번 catch할 수 있게 합니다.
-      throw error;
-    });
-}; */
-
-// redux-promise-middleware
-export const getPost = postId => ({
-  type: GET_POST,
-  payload: getPostAPI(postId)
-});
+/* redux-pender의 액션 구조는 Flux standard action(https://github.com/redux-utilities/flux-standard-action)
+   을 따르기 떄문에, createAction으로 액션을 만들 수 있습니다.
+   두 번째로 들어가는 파라미터는 Promise를 반환하는 함수여야 합니다.
+*/
+export const getPost = createAction(GET_POST, getPostAPI);
 
 // 리듀서 함수 구현
 const initialState = {
-  pending: false,
-  error: false,
+  // 요청이 진행중인지, 오류가 발생했는지 여부는 더 이상 직접 관리할 필요가 없습니다.
+  // penderREducer가 담당하기 때문이죠.
+  // pending: false,
+  // error: false,
   data: {
     title: "",
     body: ""
   }
 };
 
-export default handleActions(
-  {
-    [GET_POST_PENDING]: (state, action) => {
-      return {
-        ...state,
-        pending: true,
-        error: false
-      };
-    },
-    [GET_POST_SUCCESS]: (state, action) => {
-      const { title, body } = action.payload.data;
+// export default handleActions(
+//   {
+//     // 비동기 작업을 redux-pender로 관리할 때는 ...pender 사용.
+//     // 비동기 작업 여러 개 관리시 ...pender 여러 번 사용. 혹은 applyPenders 함수 사용
+//     ...pender({
+//       type: GET_POST, // type이 주어지면 이 type에 접미사를 붙인 액션 핸들러들이 담긴 객체를 만듭니다.
+//       /* 요청중일 때와 실패했을 때 추가로 해야 할 작업이 있다면 onPending과 onFailure를 추가하면 됩니다.
+//          onPending: (state, action) => state,
+//          onFailure: (state, action) => state
+//       */
+//       onSuccess: (state, action) => {
+//         // 성공했을 때 해야 할 작업이 따로 없으면 생략
+//         const { title, body } = action.payload.data;
+//         return {
+//           data: {
+//             title,
+//             body
+//           }
+//         };
+//       }
+//       // 함수를 생략했을 때 기본 값으로는 (state, action) => state를 설정합니다.
+//       // state를 그대로 반환
+//     })
+//   },
+//   initialState
+// );
 
+const reducer = handleActions(
+  {
+    // 다른 일반 액션들을 관리...
+  },
+  initialState
+);
+
+export default applyPenders(reducer, [
+  {
+    type: GET_POST,
+    onSuccess: (state, action) => {
+      // 성공했을 때 해야 할 작업이 따로 없으면 생략
+      const { title, body } = action.payload.data;
       return {
-        ...state,
-        pending: false,
         data: {
           title,
           body
         }
       };
     },
-    [GET_POST_FAILURE]: (state, action) => {
+    onCancel: (state, action) => {
       return {
-        ...state,
-        pending: false,
-        error: true
+        data: {
+          title: "취소",
+          body: "취소"
+        }
       };
     }
-  },
-  initialState
-);
+  }
+  /* 다른 pender 액션들
+  { type: GET_SOMETHING, onSuccess: (state, action)=> ... },
+  { type: GET_SOMETHING, onSuccess: (state, action)=> ... },
+  */
+]);
