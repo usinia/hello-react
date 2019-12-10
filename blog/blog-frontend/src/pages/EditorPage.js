fc95fbbcd7146ef9056c89468b24bb8a773810df
usinia <usinia0103@gmail.com>
2019-12-10 14:11:39 +0900

    20장 블로그 프로젝트 - 마크다운
    codemirror css 오류

--- blog/blog-frontend/src/pages/EditorPage.js
+++ blog/blog-frontend/src/pages/EditorPage.js
@@ -1,7 +1,19 @@
 import React from "react";
+import EditorTemplate from "components/editor/EditorTemplate";
+import EditorHeader from "components/editor/EditorHeader/EditorHeader";
+// import EditorPane from "components/editor/EditorPane/EditorPane";
+// import PreviewPane from "components/editor/PreviewPane/PreviewPane";
+import EditorPaneContainer from "containers/editor/EditorPaneContainer";
+import PreviewPaneContainer from "containers/editor/PreviewPaneContainer";
 
 const EditorPage = () => {
-  return <div>Editor</div>;
+  return (
+    <EditorTemplate
+      header={<EditorHeader />}
+      editor={<EditorPaneContainer />}
+      preview={<PreviewPaneContainer />}
+    />
+  );
 };
 
 export default EditorPage;
--- blog/blog-frontend/src/pages/PostPage.js
+++ blog/blog-frontend/src/pages/PostPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PageTemplate from "components/common/PageTemplate";
 import PostInfo from "components/post/PostInfo";
-import PostBody from "../components/post/PostBody";
+import PostBody from "components/post/PostBody";
 
 const PostPage = () => {
   return (
