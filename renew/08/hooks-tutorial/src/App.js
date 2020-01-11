d455088fa7493e5b5488f260e4c43162aab22542
usinia <usinia0103@gmail.com>
2020-01-11 12:53:28 +0900

    8.3 useReducer

---
+++ renew/08/hooks-tutorial/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Counter from "./Counter";
+import Info from "./Info";
+
+function App() {
+  return (
+    <div>
+      <Counter />
+      <Info />
+    </div>
+  );
+}
+
+export default App;
---
+++ renew/08/hooks-tutorial/src/Info.js
@@ -0,0 +1,40 @@
+import React, { useReducer } from "react";
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value
+  };
+}
+
+const Info = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    name: "",
+    nickname: ""
+  });
+  const { name, nickname } = state;
+  const onChange = e => {
+    dispatch(e.target); /* e.target 자체를 액션으로 사용 */
+  };
+
+  return (
+    <div>
+      <div>
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
+      </div>
+      <div>
+        <div>
+          <b>이름: </b>
+          {name}
+        </div>
+        <div>
+          <b>닉네임: </b>
+          {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
